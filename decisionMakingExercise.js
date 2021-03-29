4cc1834810eeeebf1f7f5ccf91ed6e42fdb482b2
Isaiah Robinson <isaiahrobinson@KCCCs-MacBook-Pro-8.local>
2021-03-29 02:30:58 -0400

    Completed Decision Making exercise

---
+++ decisionMakingExercise.js
@@ -0,0 +1,23 @@
+let randomNum = Math.floor(Math.random() * 5) + 1;
+console.log(randomNum);
+
+if (randomNum >= 4) {
+
+    console.log("Greater than or equal to 4");
+} else if (randomNum >= 2) {
+    console.log("Equal to 2 or 3");
+} else {
+    console.log("Equal to 1");
+}
+
+if (randomNum != 3) {
+    console.log("Not equal to 3");
+}
+
+if (randomNum !== (3 && 5)) {
+    console.log("NOT equal to 3 AND not equal to 5");
+}
+
+if (randomNum === (2 || 4)) {
+    console.log("Equal to 2 OR equal to 4");
+}
